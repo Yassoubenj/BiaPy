ea78c32b9b915e990ec0854c6a2143a54cde8db2
Yassoubenj <yasminebenjelloun@outlook.be>
2025-05-14 18:34:04 +0200

    syntax error

--- biapy/engine/semantic_seg.py
+++ biapy/engine/semantic_seg.py
@@ -14,7 +14,7 @@ from biapy.engine.metrics import (
     CrossEntropyLoss_wrapper,
     DiceBCELoss,
     DiceLoss,
-    SoftClDiceLoss,
+    SoftclDiceLoss,
     )
 from biapy.data.dataset import PatchCoords
 
@@ -148,8 +148,8 @@ class Semantic_Segmentation_Workflow(Base_Workflow):
         elif self.cfg.LOSS.TYPE == "CLDICE": #voir paramètre à mettre dans config 
             iter_  = getattr(self.cfg.LOSS, "ITER", 3)
             smooth = getattr(self.cfg.LOSS, "SMOOTH", 1.0)
-            nclass = self.cfg.MODEL.N_CLASSES
-            self.loss = SoftClDiceLoss(iter_=iter_, smooth=smooth, from_logits=True) 
+            #nclass = self.cfg.MODEL.N_CLASSES
+            self.loss = SoftclDiceLoss(iter_=iter_, smooth=smooth) 
         #elif self.cfg.LOSS.TYPE.upper() == "MSE":
             # torch.nn.MSELoss fait la moyenne voxel à voxel
             #self.loss = nn.MSELoss(reduction='mean')
