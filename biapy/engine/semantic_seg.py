d059f070f1213bc062c7ad1dc325eda643921e2b
Yassoubenj <yasminebenjelloun@outlook.be>
2025-11-12 11:22:42 +0100

    trainingn with dice 60 et cldice 40

--- biapy/engine/semantic_seg.py
+++ biapy/engine/semantic_seg.py
@@ -171,7 +171,7 @@ class Semantic_Segmentation_Workflow(Base_Workflow):
         elif self.cfg.LOSS.TYPE == "W_CE_DICE":
             self.loss = DiceBCELoss(w_dice=self.cfg.LOSS.WEIGHTS[0], w_bce=self.cfg.LOSS.WEIGHTS[1])
         elif self.cfg.LOSS.TYPE == "CLDICE":
-            self.loss = SoftclDiceLoss3D_annealed(iter_=20, smooth=1e-7, iter_min=3, anneal_steps=54000)
+            self.loss = SoftclDiceDiceBCELoss(alpha_cldice=0.4, alpha_dice=0.6, iter_=10, smooth=0.0000001)
         #elif self.cfg.LOSS.TYPE == "CLDICE": 
         #    self.loss = SoftclDiceDiceBCELoss(alpha_cldice=1.0, alpha_dice=0.0, iter_=2, smooth=0.0000001)
             #clDICE- BCE :
